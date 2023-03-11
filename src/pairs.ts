aac026a3e81bc1eb4af2e20851aee4adc90dbc63
sina <spsina@pop-os.localdomain>
2023-03-11 09:58:11 +0330

    handle pair created

--- src/pairs.ts
+++ src/pairs.ts
@@ -1,19 +1,183 @@
-import { Address, Bytes } from "@graphprotocol/graph-ts";
-import { PairCreated } from "../generated/Router/PairFactory";
+import { Address, BigInt, Bytes } from "@graphprotocol/graph-ts";
+import { PairCreated } from "../generated/PairFactoryUpgradeable/PairFactory";
+import { Pair as ThePair } from "../generated/PairFactoryUpgradeable/Pair";
+
 import {
-  PairEdge,
-  PairTree,
+  Pair,
   PathToTarget,
-  TokenToEdge,
+  TokenData,
+  TokenToPair,
 } from "../generated/schema";
 
-export function handlePairCreated(event: PairCreated): void {
-  // add the pair to a tree structure with a path to BNB as root
-  // this will allow us to get the path from any token to BNB
-  // and then we can get the path from any token to any other token
-  // by combining the paths
-  // this will allow us to get the volume of any token in BNB
+const WBNB = Address.fromString("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c");
 
+const USDC = Address.fromString("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d");
+
+const BUSD = Address.fromString("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56");
+
+const HEY = Address.fromString("0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5");
+
+const FRAX = Address.fromString("0x90C97F71E18723b0Cf0dfa30ee176Ab653E89F40");
+
+const TOKEN_DATA_ID = "TOKEN_DATA";
+
+const targets = [WBNB, USDC, BUSD, HEY, FRAX];
+
+function addEdge(token: Address, pair: Pair): void {
+  let tokenToPair = TokenToPair.load(token.toHex());
+  if (!tokenToPair) {
+    tokenToPair = new TokenToPair(token.toHex());
+    tokenToPair.pairs = [];
+  }
+  const pairs = tokenToPair.pairs;
+  pairs.push(pair.id);
+  tokenToPair.pairs = pairs;
+  tokenToPair.save();
+}
+
+function addToken(token: Address): void {
+  // adds the token to TokenData scheme if not exists
+  let tokenData = TokenData.load(TOKEN_DATA_ID);
+  if (!tokenData) {
+    tokenData = new TokenData(TOKEN_DATA_ID);
+    tokenData.tokens = [];
+  }
+
+  const tokens = tokenData.tokens;
+
+  if (!tokens.includes(token)) {
+    tokens.push(token);
+  }
+
+  tokenData.tokens = tokens;
+  tokenData.save();
+}
+
+function getTokens(): Address[] {
+  let tokenData = TokenData.load(TOKEN_DATA_ID);
+  if (!tokenData) {
+    tokenData = new TokenData(TOKEN_DATA_ID);
+    tokenData.tokens = [];
+  }
+  return tokenData.tokens.map<Address>((bytesAddress) =>
+    Address.fromBytes(bytesAddress)
+  );
+}
+
+function getPairs(token: Address): Pair[] {
+  let tokenToPair = TokenToPair.load(token.toHex());
+  if (!tokenToPair) {
+    return [];
+  } else {
+    return tokenToPair.pairs
+      .map<Pair>((strPairAddress) => Pair.load(strPairAddress)!)
+      .filter(
+        (pair): bool => {
+          const thePair = ThePair.bind(Address.fromString(pair.id));
+          const reserves = thePair.getReserves();
+
+          return !(
+            // low liquidity
+            (
+              reserves.value0.lt(BigInt.fromI64(10000000000)) ||
+              reserves.value1.lt(BigInt.fromI64(10000000000))
+            )
+          );
+        }
+      );
+  }
+}
+
+function savePathToTarget(
+  token: Address,
+  target: Address,
+  path: Address[]
+): void {
+  const id = token.toHex() + "-" + target.toHex();
+  let pathToTarget = PathToTarget.load(id);
+  if (!pathToTarget) {
+    pathToTarget = new PathToTarget(id);
+    pathToTarget.token = token;
+    pathToTarget.target = target;
+    pathToTarget.path = [];
+  }
+
+  pathToTarget.path = changetype<Bytes[]>(path);
+  pathToTarget.save();
+}
+
+class IncomingPair {
+  public parent: IncomingPair | null;
+  public token: Address;
+  public pair: Pair;
+
+  constructor(parent_: IncomingPair | null, token_: Address, pair_: Pair) {
+    this.parent = parent_;
+    this.token = token_;
+    this.pair = pair_;
+  }
+}
+
+function PairsToIncomingPairs(
+  parent: IncomingPair | null,
+  token: Address,
+  pairs: Pair[]
+): IncomingPair[] {
+  const _pairs: IncomingPair[] = [];
+  for (let i = 0; i < pairs.length; i++) {
+    _pairs.push(new IncomingPair(parent, token, pairs[i]));
+  }
+  return _pairs;
+}
+
+function calculatePathToTarget(token: Address, target: Address): void {
+  let visited: Map<string, bool> = new Map<string, bool>();
+
+  if (token == target) {
+    savePathToTarget(token, target, [target]);
+    return;
+  }
+
+  let fringe: Array<IncomingPair>;
+  fringe = PairsToIncomingPairs(null, token, getPairs(token));
+
+  while (fringe.length > 0) {
+    const top = fringe.shift();
+    const _token = top.token;
+    const _pair = top.pair;
+
+    visited.set(_pair.id, true);
+
+    const otherToken = _pair.token0 == _token ? _pair.token1 : _pair.token0;
+
+    if (otherToken == target) {
+      const path: Address[] = [];
+      let __parent: IncomingPair | null = top;
+      while (__parent != null) {
+        path.push(Address.fromString(__parent.pair.id));
+        __parent = __parent.parent;
+      }
+      savePathToTarget(token, target, path.reverse());
+      return;
+    }
+
+    // continue the search from otherToken
+    const otherTokenPairs = getPairs(Address.fromBytes(otherToken));
+    for (let i = 0; i < otherTokenPairs.length; i++) {
+      if (!visited.has(otherTokenPairs[i].id)) {
+        fringe.push(
+          new IncomingPair(
+            top,
+            Address.fromBytes(otherToken),
+            otherTokenPairs[i]
+          )
+        );
+      }
+    }
+  }
+}
+
+export function handlePairCreated(event: PairCreated): void {
   // get the pair address
   const pairAddress = event.params.pair;
   // get the token0 address
@@ -23,93 +187,22 @@ export function handlePairCreated(event: PairCreated): void {
 
   // create an edge and add it to the tree
   // the edge will be from token0 to token1 and the pair address will be the edge id
-  const pairEdge = new PairEdge(pairAddress.toHex());
-  pairEdge.token0 = token0Address;
-  pairEdge.token1 = token1Address;
-  pairEdge.stable = event.params.stable;
-  pairEdge.save();
+  const pair = new Pair(pairAddress.toHex());
+  pair.token0 = token0Address;
+  pair.token1 = token1Address;
+  pair.stable = event.params.stable;
+  pair.save();
 
-  // get or create TokenToEdge for each token
-  let token0ToEdge = TokenToEdge.load(token0Address.toHex());
-  if (token0ToEdge == null) {
-    token0ToEdge = new TokenToEdge(token0Address.toHex());
-    token0ToEdge.edges = [];
-  }
-  let token1ToEdge = TokenToEdge.load(token1Address.toHex());
+  // adjacency list
+  addEdge(token0Address, pair);
+  addEdge(token1Address, pair);
 
-  if (token1ToEdge == null) {
-    token1ToEdge = new TokenToEdge(token1Address.toHex());
-    token1ToEdge.edges = [];
-  }
+  addToken(token0Address);
+  addToken(token1Address);
 
-  // add the edge to the token
-  const token0Edges = token0ToEdge.edges;
-  token0Edges.push(pairEdge.id);
-  token0ToEdge.edges = token0Edges;
-  token0ToEdge.save();
+  const tokens = getTokens();
 
-  // add the edge to the token
-  const token1Edges = token1ToEdge.edges;
-  token1Edges.push(pairEdge.id);
-  token1ToEdge.edges = token1Edges;
-  token1ToEdge.save();
-
-  // get or create PairTree entity
-  let pairTree = PairTree.load("PAIR_TREE");
-  if (pairTree == null) {
-    pairTree = new PairTree("PAIR_TREE");
-    pairTree.edges = [];
-  }
-
-  // add the edge to the tree
-  const edges = pairTree.edges;
-  edges.push(pairEdge.id);
-  pairTree.edges = edges;
-  pairTree.save();
-
-  // get the path to WBNB for both tokens
-  const WBNB = Address.fromString("0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c");
-
-  const USDC = Address.fromString("0x8AC76a51cc950d9822D68b83fE1Ad97B32Cd580d");
-
-  const BUSD = Address.fromString("0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56");
-
-  const HEY = Address.fromString("0x0782b6d8c4551B9760e74c0545a9bCD90bdc41E5");
-
-  const FRAX = Address.fromString("0x90C97F71E18723b0Cf0dfa30ee176Ab653E89F40");
-
-  const targets = [WBNB, USDC, BUSD, HEY, FRAX];
-
-  calculatePathToTarget(token0Address, WBNB);
-  //   calculatePathToTarget(token1Address, WBNB);
-
-  //   savePathToTarget(token0Address, Address.fromBytes(WBNB));
-  //   savePathToTarget(token1Address, Address.fromBytes(WBNB));
-
-  //   for (let i = 0; i < targets.length; i++) {
-  //     const target = targets[i];
-  //     savePathToTarget(token0Address, Address.fromBytes(target));
-  //     savePathToTarget(token1Address, Address.fromBytes(target));
-  //   }
-}
-
-function dfs(
-  token: Address,
-  target: Address,
-  visited: Map<Address, Boolean>,
-  parents: Map<Address, Address>
-): Map<Address, Address> {
-  if (token == target) {
-    return parents; // create the path from parents
-  }
-
-  if (visited.has(token)) {
-    return parents;
-  }
-
-  visited.set(token, true);
-
-  const tokenToEdge = TokenToEdge.load(token.toHex())!;
-
-  for (let i = 0; i < tokenToEdge.edges.length; i++) {}
+  tokens.forEach((token) => {
+    calculatePathToTarget(token, WBNB);
+  });
 }
